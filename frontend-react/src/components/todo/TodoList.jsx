30d626e8bfd5f84c84c45481d6f68781e6eb8878
haminee01 <146091591+haminee01@users.noreply.github.com>
2025-07-02 12:56:19 +0900

    front

---
+++ frontend-react/src/components/todo/TodoList.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import TodoCard from "./TodoCard";
+
+const TodoList = ({ todos, currentFilter }) => {
+  const getFilteredTodos = () => {
+    switch (currentFilter) {
+      case "completed":
+        return todos.filter((todo) => todo.isCompleted);
+      case "incomplete":
+        return todos.filter((todo) => !todo.isCompleted);
+      default:
+        return todos;
+    }
+  };
+  const filteredTodos = getFilteredTodos();
+
+  return (
+    <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4">
+      {filteredTodos.map((todo) => (
+        <div key={todo.id} className="col mb-3">
+          <TodoCard todo={todo} />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default TodoList;
